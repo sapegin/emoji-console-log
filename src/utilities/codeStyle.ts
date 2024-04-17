19316bdd8235d7c9dfbbde1913e5888e36ca004b
Artem Sapegin <artem@sapegin.ru>
2024-04-17 18:09:16 +0200

    fix: Memoize code style function

--- src/utilities/codeStyle.ts
+++ src/utilities/codeStyle.ts
@@ -1,5 +1,8 @@
+import _ from 'lodash';
 import { getSupportInfo, Options, resolveConfig } from 'prettier';
-import { TextEditorOptions } from 'vscode';
+import { TextEditorOptions, window } from 'vscode';
+
+const debug = window.createOutputChannel('Emoji Console Log');
 
 export interface CodeStyle {
   tab: string;
@@ -7,24 +10,24 @@ export interface CodeStyle {
   semicolon: ';' | '';
 }
 
-// TODO: Memoize
-
 const defaultTabWidth = 2;
 
-async function getPrettierDefaultOption(optionName: string) {
+async function getPrettierDefaultOption<T>(optionName: string) {
   const info = await getSupportInfo();
-  return info.options.find(({ name }) => name === optionName)?.default;
+  return info.options.find(({ name }) => name === optionName)?.default as
+    | T
+    | undefined;
 }
 
-async function resolvePrettierOption(
+async function resolvePrettierOption<T>(
   optionName: string,
   fileConfig: Options | null,
 ) {
   if (fileConfig?.[optionName]) {
-    return fileConfig?.[optionName];
+    return fileConfig?.[optionName] as T;
   }
 
-  return getPrettierDefaultOption(optionName);
+  return getPrettierDefaultOption<T>(optionName);
 }
 
 function getTab(editorOptions: TextEditorOptions) {
@@ -39,20 +42,30 @@ function getTab(editorOptions: TextEditorOptions) {
   }
 }
 
-export async function getFileCodeStyle(
+async function getFileCodeStyleRaw(
   filepath: string,
   editorOptions: TextEditorOptions,
 ): Promise<CodeStyle> {
   const fileConfig = await resolveConfig(filepath);
-  const prettierSingleQuote = await resolvePrettierOption(
+  const prettierSingleQuote = await resolvePrettierOption<boolean>(
     'singleQuote',
     fileConfig,
   );
-  const prettierSemicolon = await resolvePrettierOption('semi', fileConfig);
+  const prettierSemicolon = await resolvePrettierOption<boolean>(
+    'semi',
+    fileConfig,
+  );
 
-  return {
+  const style: CodeStyle = {
     tab: getTab(editorOptions),
     quote: prettierSingleQuote ? "'" : '"',
     semicolon: prettierSemicolon ? ';' : '',
   };
+
+  debug.appendLine(`Detected code style for ${filepath}:`);
+  debug.appendLine(JSON.stringify(style));
+
+  return style;
 }
+
+export const getFileCodeStyle = _.memoize(getFileCodeStyleRaw);
