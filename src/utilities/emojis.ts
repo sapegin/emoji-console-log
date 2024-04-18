45dedcde4bd7de289aaf77f95efb01c89a6e52b0
Artem Sapegin <artem@sapegin.ru>
2024-04-18 18:35:40 +0200

    feat: Avoid repeating the same emojis

--- src/utilities/emojis.ts
+++ src/utilities/emojis.ts
@@ -21,6 +21,21 @@ export const emojis = [
   '🚀',
 ];
 
-export function getRandomEmoji(): string {
-  return emojis[Math.floor(Math.random() * emojis.length)];
+/**
+ * Return non-repeating random item from an array factory
+ * Source: https://stackoverflow.com/a/17891411/1973105
+ */
+function randomNoRepeats(array: string[]) {
+  let copy = [...array];
+  return () => {
+    if (copy.length < 1) {
+      copy = [...array];
+    }
+    const index = Math.floor(Math.random() * copy.length);
+    const item = copy[index];
+    copy.splice(index, 1);
+    return item;
+  };
 }
+
+export const getRandomEmoji = randomNoRepeats(emojis);
