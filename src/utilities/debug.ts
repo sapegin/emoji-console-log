8beb706808783dcf0b9009ac2ada3c013b0366b0
Artem Sapegin <artem@sapegin.ru>
2025-01-02 13:31:20 +0100

    Revert accidental name change

--- src/utilities/debug.ts
+++ src/utilities/debug.ts
@@ -6,7 +6,7 @@ const debug = window.createOutputChannel(packageJson.displayName);
 /**
  * Log debug message or data
  */
-export function logMessage(...messages: unknown[]) {
+export function logDebugMessage(...messages: unknown[]) {
   debug.appendLine(
     messages
       .map((x) =>
